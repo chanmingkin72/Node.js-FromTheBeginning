d04236f1dae1de1ad13bafa3cebac0870ea16022
RobertPrediger <robert.prediger@web4biz.de>
2016-04-22 12:35:31 +0200

    changes for koa

--- express/express.js
+++ express/express.js
@@ -10,9 +10,9 @@ app.use( favicon( __dirname + '/public/favicon.ico' ) );
 app.use( function( req, res, next ) {
     var start   = new Date;
     console.log( "way down", req.url );
-    
+
     next();
-    
+
     console.log( 'way back' );
     console.log( 'Response-Time', new Date - start );
 });
@@ -23,15 +23,10 @@ router.get( "/:table", function( req, res ) {
     res.send( "Request: " + req.params.table );
 });
 router.get( "/:table/:id", function( req, res ) {
-    res.send( req.params );
-});
-
-
-app.get( "/rest/:table", auth, function( req, res ) {
-    res.send( "Request: " + req.params.table );
-});
-app.get( "/rest/:table/:id", auth, function( req, res ) {
-    res.send( req.params );
+    process.nextTick( function() {
+        console.log( "req", req.params );
+        res.send( req.params );
+    });
 });
 
 app.get( "/", function( req, res ) {
@@ -54,10 +49,12 @@ function auth( req, res, next ) {
     
     // check for user
     if (!req.query.user) {
-        console.log( "No valid user" );
-        res.status( 404 ).send( "No valid user" );
+        console.log( "Not a valid user" );
+        res.status( 500 ).send( "Not a valid user" );
         return;
     }
-        
+
+    // valid user
+    console.log( "Valid user" );
     next();
 }
